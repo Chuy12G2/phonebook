923c3fcf3d67f3f9778cee4d1949362662d12ee7
Jesus <chuygs12@gmail.com>
2023-01-05 14:54:33 -0600

    change to receive the the persons from the json-server

--- src/App.js
+++ src/App.js
@@ -1,94 +1,104 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      phone: '000-111-222'}
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newPhone, setNewPhone] = useState('')
-  const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response);
+      setPersons(response.data);
+    });
+  }, []);
+
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleChangeName = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
-  const filteredArray = persons.filter((persona => {
-     return persona.name.toLowerCase().slice(0, filter.length) === filter.toLowerCase();
-  }))
+  const filteredArray = persons.filter((persona) => {
+    return (
+      persona.name.toLowerCase().slice(0, filter.length) ===
+      filter.toLowerCase()
+    );
+  });
 
   const handleChangePhone = (event) => {
-    setNewPhone(event.target.value)
-  }
+    setNewPhone(event.target.value);
+  };
 
   const handleChangeFilter = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
 
   const fullArrayToRender = persons.map((person) => (
-    <p key={person.name}>{person.name} - {person.phone}</p>
-  ))
+    <p key={person.name}>
+      {person.name} - {person.number}
+    </p>
+  ));
 
   const filteredArrayToRender = filteredArray.map((person) => (
-    <p key={person.name}>{person.name} - {person.phone}</p>
-  ))
+    <p key={person.name}>
+      {person.name} - {person.number}
+    </p>
+  ));
 
-  const arrayToRender = filteredArray.length === 0 ? fullArrayToRender : filteredArrayToRender;
-
+  const arrayToRender =
+    filteredArray.length === 0 ? fullArrayToRender : filteredArrayToRender;
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
+
     if (handleRepeatedValues()) {
-      return
+      return;
     }
 
     const objectToAdd = {
       name: newName,
-      phone: newPhone,
-    }
-    
-    setPersons(persons.concat(objectToAdd))
-    setNewName("")
-    setNewPhone("")
-  }
+      number: newPhone,
+    };
 
-
+    setPersons(persons.concat(objectToAdd));
+    setNewName("");
+    setNewPhone("");
+  };
 
   const handleRepeatedValues = () => {
-    const isItRepeated = persons.filter(person => person.name === newName)
+    const isItRepeated = persons.filter((person) => person.name === newName);
 
-    
-    if(isItRepeated.length !== 0){
-      alert(`${newName} already exist`)
-      return true
+    if (isItRepeated.length !== 0) {
+      alert(`${newName} already exist`);
+      return true;
     }
-    return false
-  }
-
+    return false;
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      FIlter: <input 
-        onChange={handleChangeFilter}
-        value={filter}
-        />
+      FIlter: <input onChange={handleChangeFilter} value={filter} />
       <form onSubmit={handleSubmit}>
         <div>
-          Name: <input onChange={handleChangeName} value={newName}/>
-          <br/>
-          Phone: <input onChange={handleChangePhone} value={newPhone}/>
+          Name: <input onChange={handleChangeName} value={newName} />
+          <br />
+          Phone: <input onChange={handleChangePhone} value={newPhone} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{(filteredArray.length === 0 && filter) ? "No coincidences found" : arrayToRender }</div>
-      
+      <div>
+        {filteredArray.length === 0 && filter
+          ? "No coincidences found"
+          : arrayToRender}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
